d6b65b494e2e9c98713303f36a394e593c30c3e9
charanrajls <charanraj.l@tataelxsi.co.in>
2023-01-06 16:29:00 +0530

    add config

--- android/app/src/main/java/com/test/MainApplication.java
+++ android/app/src/main/java/com/test/MainApplication.java
@@ -35,6 +35,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+          @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
       };
 
   private final ReactNativeHost mNewArchitectureNativeHost =
